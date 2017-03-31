0b7cb37ced7fa80cd764bea5561703f9798e8298
Dusan Klinec <dusan.klinec@gmail.com>
2017-03-31 18:23:43 +0200

    Ad tiles text fix + code clean

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -104,7 +104,7 @@
                 @if (Gate::allows('is-admin'))
                     <a href="{{ url('/services-edit') }}" class="btn btn-default" target="_blank">
                         <i class="fa fa-th-large fa-5x"></i><br/>
-                        <br>Change tiles</a>
+                        <br>Add tiles</a>
                 @endif
 
             <hr class="separator">
@@ -140,33 +140,3 @@
 </body>
 </html>
 
-
-{{--<body>--}}
-{{--<div class="flex-center position-ref full-height">--}}
-    {{--@if (Route::has('login'))--}}
-        {{--<div class="top-right links">--}}
-            {{--@if (Auth::check())--}}
-                {{--<a href="{{ url('/home') }}">Home</a>--}}
-            {{--@else--}}
-                {{--<a href="{{ url('/login') }}">Login</a>--}}
-                {{--<a href="{{ url('/register') }}">Register</a>--}}
-            {{--@endif--}}
-        {{--</div>--}}
-    {{--@endif--}}
-
-    {{--<div class="content">--}}
-        {{--<div class="title m-b-md">--}}
-            {{--Laravel--}}
-        {{--</div>--}}
-
-        {{--<div class="links">--}}
-            {{--<a href="https://laravel.com/docs">Documentation XX</a>--}}
-            {{--<a href="https://laracasts.com">Laracasts</a>--}}
-            {{--<a href="https://laravel-news.com">News</a>--}}
-            {{--<a href="https://forge.laravel.com">Forge</a>--}}
-            {{--<a href="https://github.com/laravel/laravel">GitHub</a>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
-{{--</body>--}}
-{{--</html>--}}
